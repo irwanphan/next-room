28b3af1304735fad55b12f34c516a9e22a0c5557
@irwanphan <irwanphan@gmail.com>
2021-08-18 23:19:56 +0700

    test router query

--- pages/places/[placeid].js
+++ pages/places/[placeid].js
@@ -1,4 +1,14 @@
+import { useRouter } from 'next/router'
+
 function PlaceDetailPage() {
+    const router = useRouter()
+
+    console.log(router.pathname)
+    console.log(router.query)
+
+    // send request to backend server
+    // fetch data with id of router.query.projectId
+    
     return <section>
         the place detail
     </section>
