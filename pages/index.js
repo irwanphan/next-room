838079a5a88d91d6d8f50ec9d7f1e3c066714078
@irwanphan <irwanphan@gmail.com>
2021-08-24 21:41:35 +0700

    added and test nav

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import Link from 'next/link'
+
 function HomePage() {
 	return <section>
 		home page
+
+		<ul>
+		<li>
+				<Link href='/places'>the places</Link>
+			</li>
+			<li>
+				<Link href='/hosts'>the hosts</Link>
+			</li>
+			<li>
+				<Link href='/blogs'>the blogs</Link>
+			</li>
+		</ul>
 	</section>
 }
 
