825a9238dc1bd2da04c607b0a858912c6cbeba8b
irwanphan <irwanphan@gmail.com>
2021-08-25 15:04:30 +0700

    generate list of links dynamically

--- pages/hosts/index.js
+++ pages/hosts/index.js
@@ -1,6 +1,20 @@
+import Link from 'next/link'
+
 function HostsPage() {
+    const hosts = [
+        {id: 'ved', name: 'Vedic'},
+        {id: 'ed', name: 'Edy'},
+    ]
+
     return <section>
         hosts listed
+        <ul>
+            {hosts.map((host) => (
+                <li key={host.id}>
+                    <Link href={`/hosts/${host.id}`}>{host.name}</Link>
+                </li>
+            ))}
+        </ul>
     </section>
 }
 
