f820f3a018d5570b0f1a0d3b4c8aae4fbd42fb56
irwanphan <irwanphan@gmail.com>
2021-08-25 15:19:40 +0700

    use push as link to

--- pages/hosts/[id]/index.js
+++ pages/hosts/[id]/index.js
@@ -3,9 +3,18 @@ import { useRouter } from "next/router"
 function HostPlacesPage() {
     const router = useRouter()
     console.log(router.query)
+
+    function loadEventHandler() {
+        router.push({
+            pathname: '/hosts/[id]/[hosteventid]',
+            query: {id: 'ved', hosteventid: 'eventa'}
+        })
+    }
     
     return <section>
         this host got some places
+
+        <button onClick={loadEventHandler}>Load event A</button>
     </section>
 }
 
