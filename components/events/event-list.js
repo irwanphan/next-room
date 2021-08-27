8c8dc32a318c9edf9447e0a0dfca6f88c9a5ddc4
@irwanphan <irwanphan@gmail.com>
2021-08-27 23:38:15 +0700

    event list component

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -2,12 +2,21 @@ import EventItem from "./event-item"
 
 function EventList(props) {
     // props to pass the event data to this component
-    const { items } = props.events
+    const { items } = props
 
     return <section>
         event list
         <ul>
-            {items.map( event => <EventItem /> )}
+            {items.map( (event) => (
+                <EventItem
+                    key={event.id} 
+                    id={event.id}
+                    image={event.image}
+                    title={event.title}
+                    location={event.location}
+                    date={event.date}
+                />
+            ) )}
         </ul>
     </section>
 }
