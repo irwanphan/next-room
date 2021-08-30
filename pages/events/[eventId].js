df236eb90ea1fbea3cf0d7f3dded9f2cb8d1eda2
irwanphan <irwanphan@gmail.com>
2021-08-30 18:21:50 +0700

    added event details & icons, failed const address on event logistics, unknown err

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,21 +1,37 @@
 import { useRouter } from "next/router"
+import { Fragment } from "react"
 import { getEventById } from "../dummy-data"
+
+import EventSummary from "../../components/event-detail/event-summary"
+import EventLogistics from "../../components/event-detail/event-logistics"
+import EventContent from "../../components/event-detail/event-content"
 
 function EventDetailPage() {
     const router = useRouter()
 
-    console.log(router.query.eventId)
+    // console.log(router.query.eventId)
     const eventId = router.query.eventId
     const event = getEventById(eventId)
+    // console.log(event)
 
-    console.log(event)
     if (!event) {
         return <p>no event found</p>
     }
     
-    return <section>
-        event detail page
-    </section>
+    return (
+        <Fragment>
+            <EventSummary title={event.title}></EventSummary>
+            <EventLogistics
+                date={event.date}
+                address={event.address}
+                image={event.image}
+                imageAlt={event.title}
+            ></EventLogistics>
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
+    )
 }
 
 export default EventDetailPage
