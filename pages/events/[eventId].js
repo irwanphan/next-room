98d5b1926ddff08eb49ac7b11e721ccc9c53ec74
irwanphan <irwanphan@gmail.com>
2021-08-30 17:58:54 +0700

    find event using router hook

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,18 @@
+import { useRouter } from "next/router"
+import { getEventById } from "../dummy-data"
+
 function EventDetailPage() {
+    const router = useRouter()
+
+    console.log(router.query.eventId)
+    const eventId = router.query.eventId
+    const event = getEventById(eventId)
+
+    console.log(event)
+    if (!event) {
+        return <p>no event found</p>
+    }
+    
     return <section>
         event detail page
     </section>
