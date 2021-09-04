92ba726aadb9356060e8252f51fd14dad567dff1
irwanphan <irwanphan@gmail.com>
2021-09-04 10:44:17 +0700

    added search style

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,35 +1,38 @@
 import Button from "../ui/button"
+import classes from "./events-search.module.css"
 
 function EventsSearch() {
-    return <form action="">
-        <div>
-            <div>
-                <label htmlFor="year">year</label>
-                <select name="" id="year">
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                </select>
+    return (
+        <form className={classes.form}>
+            <div className={classes.controls}>
+                <div className={classes.control}>
+                    <label htmlFor="year">year</label>
+                    <select name="" id="year">
+                        <option value="2021">2021</option>
+                        <option value="2022">2022</option>
+                    </select>
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="month">month</label>
+                    <select name="" id="month">
+                        <option value="1">Jan</option>
+                        <option value="2">Feb</option>
+                        <option value="3">Mar</option>
+                        <option value="4">Apr</option>
+                        <option value="5">May</option>
+                        <option value="6">Jun</option>
+                        <option value="7">Jul</option>
+                        <option value="8">Aug</option>
+                        <option value="9">Sep</option>
+                        <option value="10">Oct</option>
+                        <option value="11">Nov</option>
+                        <option value="12">Dec</option>
+                    </select>
+                </div>
             </div>
-            <div>
-                <label htmlFor="month">month</label>
-                <select name="" id="month">
-                    <option value="1">Jan</option>
-                    <option value="2">Feb</option>
-                    <option value="3">Mar</option>
-                    <option value="4">Apr</option>
-                    <option value="5">May</option>
-                    <option value="6">Jun</option>
-                    <option value="7">Jul</option>
-                    <option value="8">Aug</option>
-                    <option value="9">Sep</option>
-                    <option value="10">Oct</option>
-                    <option value="11">Nov</option>
-                    <option value="12">Dec</option>
-                </select>
-            </div>
-        </div>
-        <Button></Button>
-    </form>
+            <Button link={}>find events</Button>
+        </form>
+    )
 }
 
 export default EventsSearch
