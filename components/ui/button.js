eb27f34a8bbc5f89e8eb5f6bb3170fb87fde675b
irwanphan <irwanphan@gmail.com>
2021-09-04 10:39:38 +0700

    search comp

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,11 +2,16 @@ import Link from "next/link"
 import classes from "./button.module.sass"
 
 function Button(props) {
-    return <Link href={props.link}>
-        <a className={classes.button}>
-            {props.children}
-        </a>
-    </Link>
+    // if link props is not exist, return a normal button
+    if (props.link) {
+        return <Link href={props.link}>
+            <a className={classes.button}>
+                {props.children}
+            </a>
+        </Link>
+    }
+
+    return <button className={classes.button} onClick={props.onClick}>{props.children}</button>
 }
 
 export default Button
