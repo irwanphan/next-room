6b8bcbdc06264c426f5b17e15deb10d19cd594f5
irwanphan <irwanphan@gmail.com>
2021-09-26 17:57:52 +0700

    added alert to eventid page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -5,6 +5,7 @@ import { getEventById } from "../dummy-data"
 import EventSummary from "../../components/event-detail/event-summary"
 import EventLogistics from "../../components/event-detail/event-logistics"
 import EventContent from "../../components/event-detail/event-content"
+import ErrorAlert from "../../components/ui/error-alert"
 
 function EventDetailPage() {
     const router = useRouter()
@@ -15,7 +16,11 @@ function EventDetailPage() {
     // console.log(event)
 
     if (!event) {
-        return <p>no event found</p>
+        return (
+            <ErrorAlert>
+                <p>no event found . . .</p>
+            </ErrorAlert>            
+        )
     }
     
     return (
